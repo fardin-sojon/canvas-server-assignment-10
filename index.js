9565b330634679628f58396b9a54539aad69f5b1
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-11-12 00:47:40 +0600

    added feature

--- index.js
+++ index.js
@@ -29,22 +29,30 @@ async function run() {
 
     const db = client.db('canvas-db')
     const canvasCollection = db.collection('canvas')
+    favoritesCollection = db.collection('favorites');
 
-    app.get('/recent-artwork', async(req, res)=>{
-      const cursor = canvasCollection.find().sort({createdAt: -1}).limit(6)
+    app.get('/recent-artwork', async (req, res) => {
+      const cursor = canvasCollection.find().sort({ createdAt: -1 }).limit(6)
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/artwork', async(req, res)=>{
-      const cursor = canvasCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get('/artwork', async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.artistEmail = email;
+      }
 
-    app.get('/artwork/:id', async(req, res)=>{
+      const cursor = canvasCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.get('/artwork/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await canvasCollection.findOne(query)
       res.send(result)
     })
@@ -54,6 +62,64 @@ async function run() {
       const result = await canvasCollection.insertOne(newArtwork)
       res.send(result)
     })
+
+    app.delete('/artwork/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await canvasCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+    app.patch('/artwork/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateArtwork = req.body;
+      const query = { _id: new ObjectId(id) };
+
+      const update = {
+        $set: updateArtwork,
+      };
+
+      const result = await canvasCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+
+
+    app.patch('/artwork/:id/like', async (req, res) => {
+      const id = req.params.id;
+      const result = await canvasCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { likes: 1 } }
+      );
+      res.send(result);
+    });
+
+
+    app.post('/favorites', async (req, res) => {
+      const { artworkId, userEmail } = req.body;
+      const exists = await favoritesCollection.findOne({ artworkId, userEmail });
+      if (exists) return res.status(400).send({ message: 'Already in favorites' });
+      const result = await favoritesCollection.insertOne({ artworkId, userEmail, addedAt: new Date() });
+      res.send(result);
+    });
+
+
+    app.delete('/favorites', async (req, res) => {
+      const { artworkId, userEmail } = req.body;
+      const result = await favoritesCollection.deleteOne({ artworkId, userEmail });
+      res.send(result);
+    });
+
+    app.get('/favorites/:email', async (req, res) => {
+      const email = req.params.email;
+      const favs = await favoritesCollection.find({ userEmail: email }).toArray();
+      const artworkIds = favs.map(f => new ObjectId(f.artworkId));
+      const artworks = await canvasCollection.find({ _id: { $in: artworkIds } }).toArray();
+      res.send(artworks);
+    });
+
 
 
     // Send a ping to confirm a successful connection
